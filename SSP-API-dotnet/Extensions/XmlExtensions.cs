d7a5814e1f0d62cf4d406c2389cdd5e9fbc7c2aa
Valeriy <valeriy.elykomov@gmail.com>
2024-02-02 15:34:10 +0500

    Update XmlExtensions
    Create DeserializeXml method and validation methods

--- SSP-API-dotnet/Extensions/XmlExtensions.cs
+++ SSP-API-dotnet/Extensions/XmlExtensions.cs
@@ -1,4 +1,6 @@
-﻿using System.Xml.Serialization;
+﻿using System.Xml.Linq;
+using System.Xml.Schema;
+using System.Xml.Serialization;
 
 namespace SSP_API.Extensions
 {
@@ -15,6 +17,46 @@ namespace SSP_API.Extensions
             var data = ms.GetStringFromMemoryStream();
 
             return data;
+        }
+
+        public static T DeserializeXml<T>(this string xml)
+        {
+            using var sr = new StringReader(xml);
+
+            var xmlSerializer = new XmlSerializer(typeof(T));
+
+            var schemaModel = xmlSerializer.Deserialize(sr);
+
+            return (T)schemaModel!;
+        }
+
+        public static bool Validate(this XmlSchemaSet schemaSet, string xmlData)
+        {
+            try
+            {
+                var xDoc = XDocument.Parse(xmlData);
+
+                xDoc.Validate(schemaSet, ValidationEventHandler);
+            }
+            catch (XmlSchemaValidationException ex)
+            {
+                Console.WriteLine(ex.Message);
+
+                return false;
+            }
+
+            return true;
+        }
+
+        public static void ValidationEventHandler(object sender, ValidationEventArgs e)
+        {
+            switch (e.Severity)
+            {
+                case XmlSeverityType.Error:
+                    throw new XmlSchemaValidationException($"Error schema: {e.Message}");
+                case XmlSeverityType.Warning:
+                    throw new XmlSchemaValidationException($"Warning schema: {e.Message}");
+            }
         }
     }
 }
