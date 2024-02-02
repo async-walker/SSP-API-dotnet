2dea86a48a4b4b617c6b12bbb1ad1a0a5fec4c14
Valeriy <valeriy.elykomov@gmail.com>
2024-02-02 10:33:27 +0500

    Create extensions

---
+++ SSP-API-dotnet/Extensions/StreamExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Text;
+
+namespace SSP_API.Extensions
+{
+    internal static class StreamExtensions
+    {
+        public static string GetStringFromMemoryStream(this MemoryStream stream)
+        {
+            var byteData = stream.ToArray();
+
+            var requestData = Encoding.UTF8.GetString(byteData);
+
+            return requestData;
+        }
+    }
+}
---
+++ SSP-API-dotnet/Extensions/XmlExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Xml.Serialization;
+
+namespace SSP_API.Extensions
+{
+    internal static class XmlExtensions
+    {
+        public static string SerializeToXml<T>(this T element)
+        {
+            using var ms = new MemoryStream();
+
+            var xmlSerializer = new XmlSerializer(typeof(T));
+
+            xmlSerializer.Serialize(ms, element);
+
+            var data = ms.GetStringFromMemoryStream();
+
+            return data;
+        }
+    }
+}
