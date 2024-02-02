c0cb491aa8a4d5482b72a76753ec50b96d4b50f5
Valeriy <valeriy.elykomov@gmail.com>
2024-02-02 16:57:24 +0500

    Create RestClientExtensions

---
+++ SSP-API-dotnet/Extensions/RestClientExtensions.cs
@@ -0,0 +1,22 @@
+﻿using RestSharp;
+
+namespace SSP_API.Extensions
+{
+    internal static class RestClientExtensions
+    {
+        public static async Task<RestResponse> GetResponseAsync(
+            this RestClient client, 
+            RestRequest request)
+        {
+            var response = await client.ExecuteAsync(request);
+
+            if (!response.IsSuccessStatusCode)
+                throw new HttpRequestException();
+
+            if (response.Content is null)
+                throw new ArgumentNullException(nameof(response.Content));
+
+            return response;
+        }
+    }
+}
