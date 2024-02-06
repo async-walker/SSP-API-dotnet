9e1b138e6e3c4a882aeaa7f800ca10de355d84fc
Valeriy <valeriy.elykomov@gmail.com>
2024-02-06 17:58:12 +0500

    Create OKV enum

---
+++ SSP-API-dotnet/Types/Enums/OKV.cs
@@ -0,0 +1,33 @@
+﻿namespace SSP_API.Types.Enums
+{
+    /// <summary>
+    /// Общероссийский классификатор валют
+    /// </summary>
+    public enum OKV
+    {
+        /// <summary>
+        /// Российский рубль
+        /// </summary>
+        RUB,
+        /// <summary>
+        /// Узбекский сум
+        /// </summary>
+        UZS,
+        /// <summary>
+        /// Евро
+        /// </summary>
+        EUR,
+        /// <summary>
+        /// Доллар США
+        /// </summary>
+        USD,
+        /// <summary>
+        /// Тенге
+        /// </summary>
+        KZT,
+        /// <summary>
+        /// Сом
+        /// </summary>
+        KGS
+    }
+}
