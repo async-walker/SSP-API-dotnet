84113c38ffa7ffb8d4d4393663d03e776c1809b3
Valeriy <valeriy.elykomov@gmail.com>
2024-02-20 16:03:22 +0500

    Update ISspClient.cs

--- SSP-API-dotnet/ISspClient.cs
+++ SSP-API-dotnet/ISspClient.cs
@@ -10,13 +10,14 @@ namespace SSP_API
         /// <summary>
         /// Направление запроса на получение ССП
         /// </summary>
-        /// <returns></returns>
+        /// <param name="source">Источник данных с подписью</param>
+        /// <returns>Экземпляр <see cref="RequestResult"/> с результатом запроса</returns>
         Task<RequestResult> SendRequestAsync(Stream source);
         /// <summary>
         /// Получение отчёта
         /// </summary>
         /// <param name="answerId">Идентификатор ответа</param>
-        /// <returns></returns>
+        /// <returns>Экземпляр <see cref="SspInfo"/> с информацией о сумме среднемесячных платежей</returns>
         Task<SspInfo> GetAnswerAsync(string answerId);
     }
 }
